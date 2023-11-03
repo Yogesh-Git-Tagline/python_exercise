6a7e4493bf51f8e9cc467a7f26f72bc825ff4424
YogeshC <yogeshc.tagline.com>
2023-11-03 14:19:29 +0530

    modfied eg1.py

--- eg1.py
+++ eg1.py
@@ -7,40 +7,40 @@
  E. Display list without duplicate numbers'''
 
 
-numbers = [2, 4, 5, 2, 12, 44, 5, 1, 2, 3]    #given list
+numbers = [2, 4, 5, 2, 12, 44, 5, 1, 2, 3]  # given list
 
 print("A. Length of the list")
 print("B. Display first 3 numbers")
 print("C. Display sum of odd numbers")
 print("D. Number of duplicate numbers")
 print("E. Display list without duplicate numbers")
-choice = input("Select Your Choice(A/B/C/D/E):")  #here user selects the choice(what he want to get)
+choice = input("Select Your Choice(A/B/C/D/E):") # here user selects the choice(what he want to get
 
 
 if (choice == 'A'):
-    print(len(numbers))    #on choice-A,user will get length of numbers list
+    print(len(numbers))  # on choice-A,user will get length of numbers list
 
 if (choice == 'B'):
-    print(numbers[0:3])   #on choice-B user will get first 3 numbers
+    print(numbers[0:3])  # on choice-B user will get first 3 numbers
 
 if (choice == 'C'):
-    oddsum = [i for i in numbers if i % 2 == 1]  
-    print(sum(oddsum))    #on choice-C user will get sum of odd numbers
+    oddsum = [i for i in numbers if i % 2 == 1]
+    print(sum(oddsum))  # on choice-C user will get sum of odd numbers
 
 if (choice == 'D'):
-    NewList = []
-    DupItem = []
+    Items = []
+    DuplicateItems = []
 
     for i in numbers:
-        if i not in NewList:
-            NewList.append(i)
-        elif i not in DupItem:
-            DupItem.append(i)
-    print(DupItem)        #on choice-D user will get number of duplicate numbers
+        if i not in Items:
+            Items.append(i)
+        elif i not in DuplicateItems:
+            DuplicateItems.append(i)
+    print(DuplicateItems)  # on choice-D user will get number of duplicate numbers
 
 if (choice == 'E'):
-    tempList = []
+    Items = []
     for i in numbers:
-        if i not in tempList:
-            tempList.append(i)
-    print(tempList)         #on choice-E user can Display list without duplicate numbers
+        if i not in Items:
+            Items.append(i)
+    print(Items)  # on choice-E user can Display list without duplicate numbers
