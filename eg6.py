9a4f72bf5097b7906fab7e3441a014e40b6c4d93
YogeshC <yogeshc.tagline.com>
2023-11-07 14:28:29 +0530

    modified eg6

--- eg6.py
+++ eg6.py
@@ -1,15 +1,15 @@
 """Find how many number of times the substring occuranceurs in the given string."""
 
-string = "PQRQRQRQQRQ"    #given string
+my_string = "PQRQRQRQQRQ"    #given string
 substring = "QRQ"         #sub string
 
-occurance = 0   #initially it 0 but substring is found in given string then it will increase by 1
+occurrence = 0   #initially it 0 but substring is found in given string then it will increase by 1 
 
 #both loops are iterate over lenght given string
-for i in range(len(string)):
-    for j in range(len(string)):
-        if string[i:j+1] == substring: #string checking every part of itself till full part
-            occurance +=1       #when any string part is found which is equal to substring then ,
+for i in range(len(my_string)):
+    for j in range(len(my_string)):
+        if my_string[i:j+1] == substring: #string checking every part of itself till full part
+            occurrence +=1       #when any string part is found which is equal to substring then ,
                                 #it will increase occurence by 1
 
-print(occurance)   #finally we can see the number of substring occurance
+print(occurrence)   #finally we can see the number of substring occurrence
